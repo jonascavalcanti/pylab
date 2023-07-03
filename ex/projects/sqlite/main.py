4cd1bbb454175ac84c721478bde639a47f6bd195
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2023-07-03 16:04:34 -0300

    Adding google api

--- ex/projects/sqlite/main.py
+++ ex/projects/sqlite/main.py
@@ -1,35 +1,47 @@
-from services.db import DB
+# from services.db import DB
+from services.google import Google
 import sys
+import json
 
 
 def main():
 
-    db = DB(filename="./hodor.db", table='groups')
-    # TODO colar essas funçoes em um outro metodo que vai cuidar das coisas do hodor
-    db.sql_do("""CREATE TABLE IF NOT EXISTS groups (
-                                            id varchar(3) PRIMARY KEY,
-                                            google_group_name text NOT NULL,
-                                            github_id interger NOT NULL,
-                                            github_slug text NOT NULL,
-                                            github_name text NOT NULL
-                                        );
-        """)
+    # db = DB(filename="./hodor.db", table='groups')
+    # # TODO colar essas funçoes em um outro metodo que vai cuidar das coisas do hodor
+    # db.sql_do("""CREATE TABLE IF NOT EXISTS groups (
+    #                                         id varchar(3) PRIMARY KEY,
+    #                                         google_group_name text NOT NULL,
+    #                                         github_id interger NOT NULL,
+    #                                         github_slug text NOT NULL,
+    #                                         github_name text NOT NULL
+    #                                     );
+    #     """)
 
-    db.insert(dict(id=123,
-                google_group_name="francisco",
-                github_id=321,
-                github_slug="francisco",
-                github_name="francisco"))
+    # db.insert(dict(id=123,
+    #             google_group_name="francisco",
+    #             github_id=321,
+    #             github_slug="francisco",
+    #             github_name="francisco"))
 
-    print('actual', db.retrieve(123))
+    # print('actual', db.retrieve(123))
 
-    db.update(dict(id=123,
-                   google_group_name='jonas o mestre dos magos',
-                   github_id=321,
-                   github_slug='jonas_o_mestre_dos_magos',
-                   github_name='jonas o mestre dos magos'))
+    # db.update(dict(id=123,
+    #                google_group_name='jonas o mestre dos magos',
+    #                github_id=321,
+    #                github_slug='jonas_o_mestre_dos_magos',
+    #                github_name='jonas o mestre dos magos'))
 
-    print('after update', db.retrieve(123))
+    # print('after update', db.retrieve(123))
+    g = Google(credentials='./artifacts/credentials.json')
+
+    service = g.search_directory("groups")
+
+    resp = service.list(
+            customer='C0200iw23'
+        ).execute()
+
+    print(json.dumps(resp))
+
 
 if __name__ == '__main__':
     sys.exit(main())  # next section explains the use of sys.exit
